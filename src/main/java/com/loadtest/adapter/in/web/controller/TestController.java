b3f772ac75a358d9e9b0c84e49dfb8a282d90b9f
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-10-06 15:55:46 +0900

    feat: add test creation endpoint

--- src/main/java/com/loadtest/adapter/in/web/controller/TestController.java
+++ src/main/java/com/loadtest/adapter/in/web/controller/TestController.java
@@ -1,4 +1,28 @@
 package com.loadtest.adapter.in.web.controller;
 
+import com.loadtest.adapter.in.web.dto.request.TestDefinitionCreateRequest;
+import com.loadtest.adapter.in.web.dto.response.CreateTestResponse;
+import com.loadtest.application.port.in.TestUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tests")
 public class TestController {
+
+    private final TestUseCase testUseCase;
+
+    public TestController(TestUseCase testUseCase) {
+        this.testUseCase = testUseCase;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateTestResponse create(@Valid @RequestBody TestDefinitionCreateRequest request) {
+        UUID testId = testUseCase.create(request.toDomain());
+        return new CreateTestResponse(testId);
+    }
 }
