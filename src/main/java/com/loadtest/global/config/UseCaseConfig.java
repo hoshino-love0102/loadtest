264884147694b8c5c4e20ab511a41dec104d0fcf
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-10-06 15:55:59 +0900

    chore: wire use case beans via configuration

--- src/main/java/com/loadtest/global/config/UseCaseConfig.java
+++ src/main/java/com/loadtest/global/config/UseCaseConfig.java
@@ -1,4 +1,25 @@
 package com.loadtest.global.config;
 
+import com.loadtest.application.port.in.RunUseCase;
+import com.loadtest.application.port.in.TestUseCase;
+import com.loadtest.application.port.out.TestDefinitionRepository;
+import com.loadtest.application.port.out.TestRunRepository;
+import com.loadtest.application.service.RunService;
+import com.loadtest.application.service.TestService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseConfig {
+
+    @Bean
+    public TestUseCase testUseCase(TestDefinitionRepository testDefinitionRepository) {
+        return new TestService(testDefinitionRepository);
+    }
+
+    @Bean
+    public RunUseCase runUseCase(TestDefinitionRepository testDefinitionRepository,
+                                 TestRunRepository testRunRepository) {
+        return new RunService(testDefinitionRepository, testRunRepository);
+    }
 }
